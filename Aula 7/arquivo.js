8482c00f034b2e39e5b40c1b28f6ce80ce2965f4
Pedroselecto <161313320+Pedroselecto@users.noreply.github.com>
2024-11-04 15:38:16 -0300

    Update arquivo.js

--- Aula 7/arquivo.js
+++ Aula 7/arquivo.js
@@ -1,63 +1,67 @@
-const container = document.getElementById("container");
+const container = document.getElementById("container")
 
 const pega_json = async (caminho) => {
-    const resposta = await fetch(caminho)
-    const dados = await resposta.json()
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
     return dados;
 }
 
-const acha_cookie = (chave) => {
-    const linha_de_cookies = document.cookie.split(";");
-    const procurado = lista_de_cookies.find(
-        (e) => e.startsWith(`${chave}=`));
-    return procurado.split
-}
+const manipulaClick = (evento) => {
+    const id = evento.currentTarget.dataset.id
+    const nome = evento.currentTarget.dataset.nome
+    const desc = evento.currentTarget.dataset.desc
 
-const manipulaClick = (e) => {
-    const id = e.currentTarget.dataset.id;
-    const nome = e.currentTarget.dataset.nome;
+    // Criando cookies de quem foi clicado (ficam disponíveis em todo o site)
+    document.cookie = `id=${id}`
+    document.cookie = `nome=${nome}`
+    document.cookie = `desc=${desc}`
+    
+    // Session
+    sessionStorage.setItem("id", id)
+    sessionStorage.setItem("atleta", JSON.stringify(evento.currentTarget.dataset))
 
-    document.cookie = `id=${id}`;
-    document.cookie = `nome=${nome}`;
+    // Local
+    localStorage.setItem("id", id)
+    localStorage.setItem("atleta", JSON.stringify(evento.currentTarget.dataset))
 
-    window.location = `detalhes.html?id=${id}`;
+
+    window.location = `detalhes.html?id=${id}`
 }
 
 const montaCard = (atleta) => {
-    const cartao = document.createElement("article");
+    const cartao = document.createElement("div");
+    cartao.classList.add("cartao")
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
-    const descri = document.createElement("p");
+    const desc = document.createElement("p");
     const link = document.createElement("a");
-    const span_id = document.createElement("span")
+
 
     nome.innerHTML = atleta.nome;
     cartao.appendChild(nome);
-
+    
     imagem.src = atleta.imagem;
     cartao.appendChild(imagem);
 
-    descri.innerHTML = atleta.detalhes
-    cartao.appendChild(descri);
+    desc.innerHTML = atleta.detalhes;
+    cartao.appendChild(desc);
 
-    /*link.innerHTML = "Saiba mais..."
-    link.href = `detalhes.html?id=${atleta.id}&altura=${atleta.altura}`
-    cartao.appendChild(link);*/
+    // link.innerHTML = "Saiba mais..."
+    // link.href = `detalhes.html?id=${atleta.id}`
+    // cartao.appendChild(link)
 
     cartao.dataset.id = atleta.id;
     cartao.dataset.nome = atleta.nome;
-    cartao.dataset.nacimento = atleta.nascimento;
+    cartao.dataset.desc = atleta.detalhes;
 
-    container.appendChild(cartao);
+    cartao.onclick = manipulaClick;
+
+    container.appendChild(cartao)
 }
 
+
 pega_json("https://botafogo-atletas.mange.li/2024-1/all").then(
-    ( variavel ) => {
-        variavel.forEach((atleta) => montaCard(atleta))
-    console.log("Isso imprime depois")
+    (retorno) => {
+        retorno.forEach((atleta) => montaCard(atleta))
     }
 )
-
-console.log("Isso imprime antes");
-
-
